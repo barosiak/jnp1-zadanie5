73a85a7327e3a3790d64b2aac11e1eecadcde963
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2021-12-07 09:33:32 +0100

    Exceptions done

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -5,6 +5,26 @@
 #include <vector>
 #include <memory>
 
+// PYTANIE: czy te virtual są dobrze użyte?
+
+struct VirusAlreadyCreated : public virtual std::exception {
+    virtual const char *what() const noexcept {
+        return "VirusAlreadyCreated";
+    }
+};
+
+struct VirusNotFound : public virtual std::exception {
+    virtual const char *what() const noexcept {
+        return "VirusNotFound";
+    }
+};
+
+struct TriedToRemoveStemVirus : public virtual std::exception {
+    virtual const char *what() const noexcept {
+        return "TriedToRemoveStemVirus";
+    }
+};
+
 //to bym ukryła
 template <typename Virus>
 class VirusNode {
@@ -26,8 +46,8 @@ private:
 };
 
 //TODO BASIA:
-//TODO MIESZKO: wyjąteczki
+//TODO MIESZKO: 
 //CIEKAWE: iterator 1, iterator 2, get_parents, create, connect, remove (strong)
-//DONE:
+//DONE: wyjąteczki
 
 #endif //JNP1_ZADANIE5_VIRUS_GENEALOGY_H
